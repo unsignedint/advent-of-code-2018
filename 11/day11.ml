d71d8bc5f01cf955b43542f6b987a271bacc8511
Ravi Chandra <rc@braveface.nz>
2018-12-11 18:51:00 +1300

    day 11, part 2

--- 11/day11.ml
+++ 11/day11.ml
@@ -5,23 +5,8 @@ open Stdio
 (* let serial_number = 18 *)
 let serial_number = 6392
 
-
-let cell (x,y) = [
-  x  ,y;
-  x+1,y;
-  x+2,y;
-  x  ,y+1;
-  x+1,y+1;
-  x+2,y+1;
-  x  ,y+2;
-  x+1,y+2;
-  x+2,y+2
-]
-
-let cell_value m i j =
-  cell (i,j) |>
-  List.fold ~init:0 ~f:(fun acc (i',j') ->
-    acc + Matrix.access_mat m i' j')
+let cell_value m i j w h =
+  Matrix.fold_region_on_mat ~init:0 ~f:(fun acc v _ _ -> acc + v) i (i+w) j (j+h) m
 
 let update_el i j _ =
   let rack_id = i + 10 in
@@ -36,26 +21,40 @@ let create_grid =
   Matrix.iteri_on_mat ~f:update_el m ;
   m
 
-let find_best m =
-  let rec aux1 (best_i,best_j) best_val j =
-    let rec aux2 (best_i,best_j) best_val i =
-      if i = 298 then
-        aux1 (best_i,best_j) best_val (j+1)
+let find_best m region_width region_height =
+  let rec aux1 (best_i,best_j,best_val) j =
+    let rec aux2 (best_i,best_j,best_val) i =
+      if i = (300-region_width+1) then
+        aux1 (best_i,best_j,best_val) (j+1)
       else
-        let total_power = cell_value m i j in
+        let total_power = cell_value m i j region_width region_height in
         if total_power > best_val then
-          aux2 (i,j) total_power (i+1)
+          aux2 (i,j,total_power) (i+1)
         else
-          aux2 (best_i,best_j) best_val (i+1)
+          aux2 (best_i,best_j,best_val) (i+1)
     in
-    if j = 298 then
-      (best_i, best_j), best_val
+    if j = (300-region_height+1) then
+      (best_i, best_j, best_val)
     else
-      aux2 (best_i,best_j) best_val 0
+      aux2 (best_i,best_j,best_val) 0
   in
-  aux1 (0,0) 0 0
+  aux1 (0,0,0) 0
+
+let find_best_size m =
+  let rec aux (best_i,best_j,best_val,best_n) n =
+    if n = 30 then
+      (best_i,best_j,best_val,best_n)
+    else
+      let i',j',best' = find_best m n n in
+      if best' > best_val then
+        aux (i',j',best',n) (n+1)
+      else
+        aux (best_i,best_j,best_val,best_n) (n+1)
+  in
+  aux (0,0,0,3) 3
 
 let () =
   let m = create_grid in
-  let (i,j), best = find_best m in
-  printf "%d,%d %d\n" i j best
+  (* let (i,j,best) = find_best m 3 3 in *)
+  let (i,j,best,n) = find_best_size m in
+  printf "%d,%d %d (%d)\n" i j best n
