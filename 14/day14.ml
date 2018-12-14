ad7e730a9702ddfd9f696ed31d6de691f448f527
Ravi Chandra <rc@braveface.nz>
2018-12-14 23:01:06 +1300

    day 14, part 2

--- 14/day14.ml
+++ 14/day14.ml
@@ -10,6 +10,8 @@ open Base
 open Stdio
 
 let input = 793061
+let input_ary = Int.to_string input |> String.to_array |> Array.map ~f:(fun c -> Char.to_int c - 0x30)
+let input_ary_len = Array.length input_ary
 
 let mod_counter _ start_idx step len =
   let result = (start_idx + step) % len in
@@ -48,10 +50,40 @@ let repeat f a count =
       aux a' idx1' idx2' (n+1) len' in
   aux a 0 1 0 2
 
+let compare_array a b =
+  Array.fold2_exn a b ~init:true ~f:(fun acc a' b' -> if a' = b' then acc else false) ;;
+
+type found_type = Found of int | NotFound
+
+let repeat_part2 f a =
+  let rec aux a idx1 idx2 n len =
+    (* print_row a idx1 idx2 ; *)
+    let found =
+      if len > input_ary_len then
+        if compare_array (Array.sub a ~pos:(len-input_ary_len) ~len:input_ary_len) input_ary then
+          Found (len)
+        else if compare_array (Array.sub a ~pos:(len-input_ary_len-1) ~len:input_ary_len) input_ary then
+          Found (len-1)
+        else
+          NotFound
+      else
+        NotFound
+    in
+    match found with
+    | Found x ->
+      a, idx1, idx2, x
+    | NotFound ->
+      let a', idx1', idx2', len' = f a idx1 idx2 len in
+      aux a' idx1' idx2' (n+1) len' in
+  aux a 0 1 0 2
+
 let () =
   print_endline "---" ;
   (* add an extra element to the array incase the last interation has 2 digits *)
-  let a = Array.create ~len:(793061+11) 0 in
+  let a = Array.create ~len:(input*100) 0 in
   a.(0) <- 3  ; a.(1) <- 7 ;
-  let a, _, _ = repeat update a (input+10) in
-  print_end a input ;
+  (* let a, _, _ = repeat update a (input+10) in *)
+  let a, _, _, len = repeat_part2 update a in
+  printf "len = %d\n" (len-input_ary_len) ;
+  print_end a (len-input_ary_len) ;
+  print_endline "---" ;
